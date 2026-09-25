50b1f6c24ee67de0e99fba520c39e04e2b57b2df
Mason DeWitt <madewitt55@gmail.com>
2026-09-25 05:57:16 -0400

    Add hardware address checking for beacon-only connection. Cleanup code

--- rover.ino
+++ rover.ino
@@ -11,14 +11,23 @@ const bool USE_RANGE_FILTER = true; // UWB range-smoothing filter
 #define SPI_MOSI 23 // SPI data out (controller -> peripheral, purely configuration data)
 #define PIN_RST 27  // Hardware reset (controller pulls to LOW during init to reboot peripheral)
 #define PIN_IRQ 34  // Interrupt (peripheral raises to HIGH when it has a completed measurement)
-#define DW1000_HARDWARE_ADDRESS "7D:00:22:EA:82:60:3B:9C" // Unique hardware address of DW1000 UWB chip
+#define ROVER_HARDWARE_ADDRESS "7D:00:22:EA:82:60:3B:9C" // Hardware address of the onboard DW1000 UWB chip
+#define BEACON_HARDWARE_ADDRESS "86:17:5B:D5:A9:9A:E2:9C" // Hardware address of beacon DW1000 UWB chip
+#define BEACON_SHORT_ADDRESS 0x1786                        // Short address of beacon, derived from first two bytes of BEACON_HARDWARE_ADDRESS
 const uint8_t  NUM_RANGE_SAMPLES = 5; // Number of range samples to be recorded and averaged
-float rangeBuf[NUM_RANGE_SAMPLES] = {0}; // Buffer to store range values for averaging, all zeroes by default
-uint8_t rangeBufIdx = 0; // Buffer index
-bool beaconDetected = false; // Beacon DW1000 UWB chip detected
+float range_buf[NUM_RANGE_SAMPLES] = {0}; // Buffer to store range values for averaging, all zeroes by default
+uint8_t range_buf_idx = 0; // Buffer index
+bool beacon_activated = false; // Beacon DW1000 UWB chip connected
+
+// Logging functions
+inline void LOG_DEBUG(const String& msg) { Serial.print("[  DEBUG  ] "); Serial.println(msg); }
+inline void LOG_INFO(const String& msg)  { Serial.print("[  INFO   ] "); Serial.println(msg); }
+inline void LOG_WARN(const String& msg)  { Serial.print("[! WARN ! ] "); Serial.println(msg); }
+inline void LOG_ERROR(const String& msg) { Serial.print("[** ERROR **] "); Serial.println(msg); }
 
 void setup() {
     Serial.begin(9600); // Baud rate 9600 Bd
+    delay(1000);
 
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI); // Initialize SPI bus, hardwired to oboard DW1000 UWB chip
     DW1000Ranging.initCommunication(PIN_RST, PIN_PS, PIN_IRQ); // Initialize DW1000 UWB chip
@@ -29,42 +38,62 @@ void setup() {
     DW1000Ranging.attachInactiveDevice(inactiveDevice); // Called when a device hasn't been heard from (lost connection, out of range)
 
     DW1000Ranging.useRangeFilter(USE_RANGE_FILTER); // Enable or disable builtin library range-smoothing feature
-    DW1000Ranging.startAsTag(DW1000_HARDWARE_ADDRESS, DW1000.MODE_LONGDATA_RANGE_LOWPOWER); // Start UWB radio as a tag; long range low power mode
+    DW1000Ranging.startAsTag(ROVER_HARDWARE_ADDRESS, DW1000.MODE_LONGDATA_RANGE_LOWPOWER); // Start UWB radio as a tag; long range low power mode
 }
 
+unsigned long last_measurement_millis = millis();
 void loop() {
     DW1000Ranging.loop(); // Ranging library's main state machine, must be called repeatedly
 
-    Serial.println(getDistance());
+    if (beacon_activated && millis() - last_measurement_millis > 1000) {
+        LOG_DEBUG("Distance from BEACON: " + String(getRoverDistance()) + "m");
+        last_measurement_millis = millis();
+    }
 }
 
 /**
  * @brief Records a new range reading between onboard DW1000 UWB chip and connected device
  *
- * Range readings are added to `rangeBuf` to later be averaged for more accurate readings
+ * Range readings are added to `range_buf` to later be averaged for more accurate readings
 */
 void newRange() {
-    rangeBuf[rangeBufIdx] = DW1000Ranging.getDistantDevice()->getRange(); // Record range (meters), write to range buffer
-    rangeBufIdx = (rangeBufIdx + 1) % NUM_RANGE_SAMPLES; // Increment index, overflows back to 0
+    if (DW1000Ranging.getDistantDevice()->getShortAddress() != BEACON_SHORT_ADDRESS) return;
+    range_buf[range_buf_idx] = DW1000Ranging.getDistantDevice()->getRange(); // Record range (meters), write to range buffer
+    range_buf_idx = (range_buf_idx + 1) % NUM_RANGE_SAMPLES; // Increment index, overflows back to 0
 }
 /**
- * @brief Registers a new connected DW1000 UWB device
+ * @brief Activates a new connected DW1000 UWB device
  *
- * @param device New DW1000 UWB device to be registered
+ * @param device New DW1000 UWB device to be activated
 */
 void newDevice(DW1000Device *device) {
-    beaconDetected = true;
+    LOG_INFO("New device activated");
+    if (device->getShortAddress() != BEACON_SHORT_ADDRESS) {
+        LOG_WARN("New device not recognized as BEACON");
+        return;
+    }
+
+    // Beacon activated
+    LOG_INFO("BEACON activated");
+    beacon_activated = true;
 }
 /**
  * @brief Deactivates a connected DW1000 UWB device
  *
- * Sets `beaconDetected` to `false` and fills `rangeBuf` with zeroes
+ * Sets `beacon_activated` to `false` and fills `range_buf` with zeroes
  *
  * @param device Pointer to connected device
 */
 void inactiveDevice(DW1000Device *device) {
-    beaconDetected = false;
-    for (uint8_t i = 0; i < NUM_RANGE_SAMPLES; ++i) rangeBuf[i] = 0; // Clear range buffer
+    if (device->getShortAddress() != BEACON_SHORT_ADDRESS) {
+        LOG_WARN("Unrecognized device disconected");
+        return;
+    }
+
+    // Beacon deactivated
+    beacon_activated = false;
+    for (uint8_t i = 0; i < NUM_RANGE_SAMPLES; ++i) range_buf[i] = 0; // Zero out range buffer
+    LOG_ERROR("BEACON disconnected");
 }
 
 /**
@@ -74,12 +103,12 @@ void inactiveDevice(DW1000Device *device) {
  *
  * @return Distance between rover and beacon if beacon is detected, `NAN` if beacon is not detected
  */
-float getDistance() {
-    if (!beaconDetected) return NAN; // Beacon not detected, range buffer innacurate
+float getRoverDistance() {
+    if (!beacon_activated) return NAN; // Beacon not detected, range buffer innacurate
 
     // Calculate average of range buffer
     float avg = 0;
-    for (uint8_t i = 0; i < NUM_RANGE_SAMPLES; ++i) avg += rangeBuf[i];
+    for (uint8_t i = 0; i < NUM_RANGE_SAMPLES; ++i) avg += range_buf[i];
     avg /= NUM_RANGE_SAMPLES;
 
     return avg;
